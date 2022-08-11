b8ec0673ea877563cd00c9e744aafb1bb73fade2
ALI ALOBAIDI <Aliwathik932@gmail.com>
2022-08-11 20:49:29 +0300

    لوز

--- main.dart
+++ main.dart
@@ -1,10 +1,8 @@
 
 import 'dart:io';
-import 'dart:math';
 
 void main() {
   bool run = true;
-  List books = [];
   while (run) {
     print('''Main menu
 1- Display all books
@@ -17,103 +15,34 @@ Write your choice:''');
     var choice = stdin.readLineSync();
     switch (choice) {
       case '1':
-        int i = 0;
-        for (Books book in books) {
-          if (books.isEmpty==false) {
-            if (i == 0) {
-              book.displayBooks(true);
-              i++;
-            } else
-              book.displayBooks(false);
-          }
-          else{
-            print("no books to display");
-          }
-        }
+       Books.Displaybooks();
         break;
       case '2':
-        for (Books book in books) {
-          if (book.rating >= 4) book.displayBooks(true);
-        }
+        Books.Display_books_4();
+
         break;
       case '3':
-        try {
-          print("Enter the count of books want to add:-");
-          String? count = stdin.readLineSync();
-          if (count != null) {
-            int Count = int.parse(count);
-            for (int i = 1; i <= Count; i++) {
-              print('Enter book $i name');
-              String? bookname = stdin.readLineSync();
-              print('Enter book $i author');
-              String? bookauthor = stdin.readLineSync();
-              print('Enter book $i rate');
-              String? rate = stdin.readLineSync();
-              Map<String, dynamic> book = {
-                'name': bookname,
-                'author': bookauthor,
-                'rate': rate
-              };
-              Books newBook = Books.fromMap(book);
-              books.add(newBook);
-            }
-          } else {
-            print("not valid");
-          }
-        } catch (e) {
-          print('input not vaild');
-        }
+        Books.Add_books();
         break;
 
       case "4":
         {
-          try {
-            print("enter book name to update");
-            String? bookname = stdin.readLineSync();
-            print("enter new book name");
-            String? new_name = stdin.readLineSync();
-            Books book =
-                books.firstWhere((element) => element.name == bookname);
-            book.update_name = new_name!;
-            print("enter new book author");
-            String? new_author = stdin.readLineSync();
-            book.update_author = new_author!;
-            print("enter new book rating");
-            String? new_rating = stdin.readLineSync();
-            book.update_rating = new_rating!;
-          } catch (e) {
-            print("book not found");
-          }
-          break;
+         Books.update_book();
+         break;
         }
       case '5':
         {
-          try {
-            print("enter book name to delete");
-            String? bookname = stdin.readLineSync();
-            Books book =
-                books.firstWhere((element) => element.name == bookname);
-            books.remove(book);
-          } catch (e) {
-            print("book not found");
-          }
+         Books.remove_book();
           break;
         }
       case '6':
         {
-          print("enter book name to serach");
-          String? bookname = stdin.readLineSync();
-          try {
-            Books book =
-                books.firstWhere((element) => element.name == bookname);
-            book.displayBooks(true);
-          } catch (e) {
-            print("book not found");
+          Books.serach_book();
           }
         }
     }
   }
-}
+
 
 class Books {
   String name;
@@ -129,10 +58,110 @@ class Books {
         author: data['author'],
         rate: data['rate'],
       );
+  static List<Books> Bookslist=[];
+  static Displaybooks(){
+    int i = 0;
+    for (Books book in Bookslist) {
+      if (Bookslist.isEmpty==false) {
+        if (i == 0) {
+          book.displayBooks(true);
+          i++;
+        } else
+          book.displayBooks(false);
+      }
+      else{
+        print("no books to display");
+      }
+    }
+  }
   void displayBooks(bool header) {
     if (header == true) print('''bookName\t\tbookAuthor\t\trate\n''');
     print('${this.name} \t\t ${this.author}\t\t ${this.rate}');
   }
+  static Display_books_4(){
+    for (Books book in Bookslist) {
+      if (book.rating >= 4) book.displayBooks(true);
+    }
+  }
+  static Add_books(){
+    try {
+      print("Enter the count of books want to add:-");
+      String? count = stdin.readLineSync();
+      if (count != null) {
+        int Count = int.parse(count);
+        for (int i = 1; i <= Count; i++) {
+          print('Enter book $i name');
+          String? bookname = stdin.readLineSync();
+          print('Enter book $i author');
+          String? bookauthor = stdin.readLineSync();
+          print('Enter book $i rate');
+
+          String? rate = stdin.readLineSync();
+          try{
+            double float_rate= double.parse(rate!);
+            if (float_rate>5) throw Exception("must be less than 5!");
+          }
+          catch (e){
+            print("not valid rating");
+            break;
+          }
+
+          Map<String, dynamic> book = {
+            'name': bookname,
+            'author': bookauthor,
+            'rate': rate
+          };
+          Books newBook = Books.fromMap(book);
+          Bookslist.add(newBook);
+        }
+      } else {
+        print("not valid");
+      }
+    } catch (e) {
+      print('input not vaild');
+    }
+  }
+  static update_book(){
+    try {
+      print("enter book name to update");
+      String? bookname = stdin.readLineSync();
+      print("enter new book name");
+      String? new_name = stdin.readLineSync();
+      Books book =
+      Bookslist.firstWhere((element) => element.name == bookname);
+      book.update_name = new_name!;
+      print("enter new book author");
+      String? new_author = stdin.readLineSync();
+      book.update_author = new_author!;
+      print("enter new book rating");
+      String? new_rating = stdin.readLineSync();
+      book.update_rating = new_rating!;
+    } catch (e) {
+      print("book not found");
+    }
+  }
+
+  static remove_book(){
+    try {
+      print("enter book name to delete");
+      String? bookname = stdin.readLineSync();
+      Books book =
+      Bookslist.firstWhere((element) => element.name == bookname);
+      Bookslist.remove(book);
+    } catch (e) {
+      print("book not found");
+    }
+  }
+  static serach_book(){
+    print("enter book name to serach");
+    String? bookname = stdin.readLineSync();
+    try {
+      Books book =
+      Bookslist.firstWhere((element) => element.name.contains(bookname!));
+      book.displayBooks(true);
+    } catch (e) {
+      print("book not found");
+  }}
 
   double get rating {
     double rat = double.parse(this.rate);
